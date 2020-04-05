96ff7ed503b960f786b5350e83fef4314fe2d7dc
Andrés Carrasco <andres.c.k@gmail.com>
2020-04-05 23:15:06 +0200

    Make master the build branch

--- src/assets/index.tsx
+++
@@ -1,7 +0,0 @@
-export * from "./experience";
-export * from "./programming";
-export * from "./tools";
-export * from "./publications";
-export * from "./education";
-export * from "./language";
-export * from "./profile";
