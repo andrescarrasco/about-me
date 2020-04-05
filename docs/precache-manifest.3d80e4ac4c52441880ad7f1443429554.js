8ce4798eef083a30796086d04924993e8c84359f
Andrés Carrasco <andres.c.k@gmail.com>
2020-04-05 22:57:05 +0200

    Remove gh-pages and build to /docs

---
+++ docs/precache-manifest.3d80e4ac4c52441880ad7f1443429554.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c17203d91d0ce24ee7656a17b2cc164",
+    "url": "/index.html"
+  },
+  {
+    "revision": "958cf2ea71f9f5f49537",
+    "url": "/static/css/main.789974f3.chunk.css"
+  },
+  {
+    "revision": "de74c65a6f7d09c95068",
+    "url": "/static/js/2.f082c337.chunk.js"
+  },
+  {
+    "revision": "301056a7ca16ead6156b38e6917dbb28",
+    "url": "/static/js/2.f082c337.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "958cf2ea71f9f5f49537",
+    "url": "/static/js/main.56d61000.chunk.js"
+  },
+  {
+    "revision": "7a8a18c83e4f7bf69d33",
+    "url": "/static/js/runtime-main.8daae993.js"
+  },
+  {
+    "revision": "ad932fdf06c513713fac0ad0ae47ca20",
+    "url": "/static/media/profile.ad932fdf.jpg"
+  }
+]);
